312f0a555af47246da7ad94054f0818302c27bf8
Do Minh Khanh <dominhkhanh99@gmail.com>
2021-03-30 18:51:43 +0700

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,188 @@
+
+import 'package:App/home.dart';
+import 'package:flutter/material.dart';
+
+void main() => runApp(MyApp()) ;  
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return new MaterialApp(
+    home: LoginPage(),
+    );
+  }
+}
+
+
+class LoginPage extends StatefulWidget {
+  @override
+  _LoginPageState createState() => _LoginPageState();
+}
+
+class _LoginPageState extends State<LoginPage> {
+  bool _showPass = false;
+  TextEditingController _userController = new TextEditingController();
+  TextEditingController _PassController = new TextEditingController();
+  var _userNameErr = "Tai khoan khong hop le";
+  var _PassErr = "mat khau phai tren 10 ki tu";
+  var _userInvalid = false;
+  var _passInvalid = false;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        body: Container(
+          padding: EdgeInsets.fromLTRB(30, 0, 30, 0),
+          constraints: BoxConstraints.expand(),
+          color: Colors.white,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.end,
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: <Widget>[
+              Padding(
+                padding:const EdgeInsets.fromLTRB(0, 0, 0, 40),
+                child:Container(
+                width: 70,
+                height: 70,
+                padding: EdgeInsets.all(15),
+                decoration: BoxDecoration(
+                  shape: BoxShape.circle,
+                  color: Color(0xffd8d8d8)
+                ),
+                child: FlutterLogo()),
+              ),  
+            Padding(
+                padding:const EdgeInsets.fromLTRB(0, 0, 0, 60),
+                child: Text("Hello \nWellcome Back", 
+                style: TextStyle(
+                fontWeight: FontWeight.bold, 
+                color:Colors.black,
+                fontSize: 30),
+                ),
+            ),
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0 , 40 ),
+              child:TextField(
+                style: TextStyle(fontSize: 18),
+                controller: _userController,
+              decoration: InputDecoration(
+                labelText: "USERNAME",
+                errorText: _userInvalid ? _userNameErr : null,
+                labelStyle: TextStyle
+                (color: Color(0xff888888),
+                fontSize: 15)
+                ),
+            ),
+            ),
+            
+            Padding(
+             padding: const EdgeInsets.fromLTRB(0, 0, 0 , 40 ),
+              child: Stack(
+              alignment: AlignmentDirectional.centerEnd,
+              children : <Widget>[
+                
+                TextField(
+                  style: TextStyle(fontSize: 18),
+                  controller: _PassController,
+                  obscureText: !_showPass,
+                decoration: InputDecoration(
+                  labelText: "PASSWORD",
+                  errorText: _passInvalid ? _PassErr : null,
+                  labelStyle: TextStyle(
+                    color: Color(0xff888888),
+                    fontSize: 15)),
+                ),
+              GestureDetector(
+                onTap: onToggleShowPass,
+                child: Text(
+                "SHOW",
+                style: TextStyle(
+                  color: Colors.blue,
+                  fontSize: 13,fontWeight: 
+                  FontWeight.bold 
+                  ),
+                )
+              )
+                
+              ]
+            ),
+            ),
+            
+            Padding(
+              padding: const EdgeInsets.fromLTRB(0, 0, 0 , 40 ),
+              child:SizedBox(
+              width: double.infinity,
+              height: 56,
+              child: RaisedButton(
+                color: Colors.blue,
+                shape: RoundedRectangleBorder(
+                  borderRadius: BorderRadius.all(
+                  Radius.circular(8)
+                  )
+                  ),
+                onPressed: onSingInClicked,
+                child: Text("SIGN IN",style: TextStyle(color: Colors.white,fontSize: 16)),
+
+              ),
+            ),
+            ),
+            
+               Container(
+                height: 130,
+                width: double.infinity,
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      "NEW USER? SIGN UP",
+                      style: TextStyle(fontSize: 15,color:Color(0xff888888))
+                    ),
+                    Text(
+                      "FORGOT PASSWORD?",
+                      style: TextStyle(fontSize: 15,color:Colors.blue),
+                    ),
+                  ],
+                ),
+              ),
+
+            
+            ],
+          ),
+        ),
+      );
+  }
+
+  void onToggleShowPass(){
+      setState(() {
+        _showPass = ! _showPass;
+      });
+  }
+
+  void onSingInClicked(){
+      setState(() {
+          if(_userController.text.length < 10 ){
+            _userInvalid = true;
+          }
+          else{
+            _userInvalid = false;
+          }
+
+          if(_PassController.text.length < 10 ){
+            _passInvalid = true;
+          }
+          else{
+            _passInvalid = false;
+          }
+
+          if(!_userInvalid && !_passInvalid){
+             Navigator.push(context, MaterialPageRoute(builder: (context)=> HomePage() ));
+          }
+
+
+      });
+  }
+  Widget gotoHome(BuildContext context){
+    return HomePage();
+  }
+}
+
